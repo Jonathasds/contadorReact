b9e239effdcd9ac3aac3234efcac66ddf5308b2f
Jonathas <jonathadelgado@gmail.com>
2022-09-05 08:16:02 -0300

    Contador-Aulas Udemy

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import "./App.css";
+
+export default () => (
+  <div className="App">
+    <h1>Contador (React) </h1>
+    <div className=" ">
+      
+    </div>
+  </div>
+);
